5cfb9737625a0a55bf96b8107094b3d0d61f36c2
JoseWilsonFilho <143343680+JoseWilsonFilho@users.noreply.github.com>
2023-08-27 14:40:59 -0300

    Add files via upload

---
+++ Atividade Arte em ACSII.py
@@ -0,0 +1,55 @@
+print('Oi, tudo bem?')
+print('Estou com fome!')
+print('Meu animal favorito é a ovelha:')
+print('o-###-')
+print(' ||  #')
+print('Uma imagem de um cachorro...')
+print(' 0___ ')
+print(' ||| ')
+print('Eu moro em Teresina:')
+print(' __|___')
+print('|     |')
+print('|     |')
+print('|     |')      
+print('-------')
+print('ha '*4)
+print('ba' + 'na'*2)
+print('Bra' + 'sil' + '!'*10)
+print('ola' * 5)
+print('ola, ' + 'tudo bem?')
+print('#' * 30)
+print('#' * 30)
+print('#' * 30)
+print('/\ '*10)
+print(' \/'*10)
+print('Parabéns!!!!!!!')
+print('Aqui vai um dinossauro!')
+print('        ____')
+print('       /    )')
+print('      /    /')
+print('   ---    |')
+print('  /       |')
+print(' /        |')
+print('---|__|-|_|')
+print('E aqui vai um bolo! huuuum')
+print('........')
+print('iiiiiiii')
+print('########')
+print('========')
+print('########')
+
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
